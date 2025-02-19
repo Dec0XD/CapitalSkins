b58abb75f82b3a377fbd30a03b4981b6c048643f
André Coêlho <83669560+Dec0XD@users.noreply.github.com>
2025-02-19 10:36:30 -0300

    Atualizando o site com as 2 primeiras paginas que leo fez

---
+++ Arrumado/assets/js/index.js
@@ -0,0 +1,12 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navLinks = document.querySelector(".nav-links");
+  
+    if (menuToggle && navLinks) {
+      menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+      });
+    }
+  });
+  
